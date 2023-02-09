35a30f522e365552bcf509b35e3b104a396c7a98
JasonPC <jasonwong287962@gmail.com>
2023-02-08 19:49:41 -0600

    implements deletable tasks

--- src/App.js
+++ src/App.js
@@ -1,53 +1,58 @@
 import Overview from "./component/Overview";
 import React from "react";
-
+import app from './app.css';
 class App extends React.Component {
   constructor () {
     super();
    
       this.state = {
-        names: ['TASKS'],
+        task: {text: '',number: 1},
+        tasks: [],
       }
 
-      this.handleClick = this.handleClick.bind(this);
-      this.submitButton = this.submitButton.bind(this);
   }
-  submitButton(e){
+
+  handleChange = (e) =>{
     e.preventDefault();
-    const nameHolder = e.target.name.value;
-      this.setState({ 
-        names: [...this.state.names,nameHolder],
-      })
-
+    this.setState({
+      task : {
+        text: e.target.value,
+        number: this.state.task.number,
+      }
+    });
+  };
+  submitButton = (e) =>{
+    e.preventDefault();
+         this.updateState();
   }
-
-  handleClick(e){
-    const nameHolder = e.target.parentElement.name.value;
+  handleClick = (e) => {
+    e.preventDefault();
+    this.updateState();
+  }
+  updateState(){console.log(this.state.task);
     this.setState({ 
-      names: [...this.state.names,nameHolder],
+      
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {text: '', number: this.state.task.number+1},
     })
-
   }
- 
+
   render() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+   
       <div>
           <form onSubmit={this.submitButton}   >
-              <label htmlFor='name'>TEST:</label>
-              <input type='text' id="name"></input>
+              <label htmlFor='task'>TEST:</label>
+              <input type='text' id="task"  value={this.state.task.text} onChange={this.handleChange}></input>
               <input type='button' value="Submit"  onClick={this.handleClick}></input>
           </form>
       </div>
 
-      <Overview name={this.state.names}></Overview>
+      <Overview name={this.state.tasks}></Overview>
     </div>
   );
 }
-
 }
 
 // MAYBE WHEN I LEARN REACT HOOK OR SOMETHING??
--- src/component/Overview.js
+++ src/component/Overview.js
@@ -4,26 +4,40 @@ class Overview extends React.Component {
 
   constructor(props){
     super(props);
-    
+
   }
-  
+
+   deleteTask = (e) =>{
+  e.preventDefault();
+   let taskString = e.target.parentElement.textContent.substring(1);
+   let num = 0;
+  this.props.name.forEach(item => {
+    let string = taskString.substring(0,taskString.indexOf(" "));
+    if(item.number == string){
+      console.log(item);
+      this.setState({
+        tasks: this.props.name.splice(num,1),
+      })
+    }
+    num++;
+  });
+  }
+
+
   render(){
     return (
         <div>
-             <ul> {this.props.name[0]}
+             <ul> TASKS
             {this.props.name.map(
-                function namesIterator(item, i){
-                  if(i == 0){
-                  i++;
-                  return;
-                  } 
+                (item, i) => {
                   return(
-                    <li key={item+i}> {item} </li>
+                    <li key={item.text+i}> {item.number} {item.text}  
+                     <button onClick={this.deleteTask} >Delete</button>
+                    </li>
                   )
                 }
               )}
-              
-              
+             
             </ul>
         </div>
     )
